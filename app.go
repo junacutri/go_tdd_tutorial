fcbb41d4c75630c780845bcde24c28966b38fe23
Juan <jmfcurti@gmail.com>
2020-07-14 20:43:22 +0200

    Fixes

--- app.go
+++ app.go
@@ -95,6 +95,31 @@ func (a *App) createProduct(w http.ResponseWriter, r *http.Request) {
     respondWithJSON(w, http.StatusCreated, p)
 }
 
+func (a *App) updateProduct(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    id, err := strconv.Atoi(vars["id"])
+    if err != nil {
+        respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+        return
+    }
+
+    var p product
+    decoder := json.NewDecoder(r.Body)
+    if err := decoder.Decode(&p); err != nil {
+        respondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
+        return
+    }
+    defer r.Body.Close()
+    p.ID = id
+
+    if err := p.updateProduct(a.DB); err != nil {
+        respondWithError(w, http.StatusInternalServerError, err.Error())
+        return
+    }
+
+    respondWithJSON(w, http.StatusOK, p)
+}
+
 func (a *App) deleteProduct(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
     id, err := strconv.Atoi(vars["id"])
--- model.go
+++ model.go
@@ -2,7 +2,6 @@ package main
 
 import (
   "database/sql"
-  "errors"
 )
 
 type product struct {
