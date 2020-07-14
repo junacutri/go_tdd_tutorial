c40a77072f257f6c38de2431615a8fd8a4555428
Juan <jmfcurti@gmail.com>
2020-07-14 20:07:10 +0200

    Add TestEmptyTable test

--- main_test.go
+++ main_test.go
@@ -4,6 +4,9 @@ import (
   "os"
   "testing"
   "log"
+
+  "net/http"
+  "net/http/httptest"
 )
 
 var a App
@@ -39,3 +42,29 @@ const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
     price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
     CONSTRAINT products_pkey PRIMARY KEY (id)
 )`
+
+func TestEmptyTable(t *testing.T) {
+  clearTable()
+
+  req, _ := http.NewRequest("GET", "/products", nil)
+  response := executeRequest(req)
+
+  checkResponseCode(t, http.StatusOK, response.Code)
+
+  if body := response.Body.String(); body != "[]" {
+    t.Errorf("Expected an empty array. Got %s", body)
+  }
+}
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+  rr := httptest.NewRecorder()
+  a.Router.ServeHTTP(rr, req)
+
+  return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+  if expected != actual {
+    t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+  }
+}
