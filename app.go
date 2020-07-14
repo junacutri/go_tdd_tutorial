2b838d495286a9843a9a8562cca9c6ecf7e4b62f
Juan <jmfcurti@gmail.com>
2020-07-14 18:39:17 +0200

    Basic Structure

---
+++ app.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "database/sql"
+
+  "github.com/gorilla/mux"
+  _ "github.com/lib/pq"
+)
+
+type App struct {
+  Router  *mux.Router
+  DB      *sql.DB
+}
+
+func (a *App) Initialize(user, password, dbname string) { }
+func (a *App) Run(addr string) {}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/juancurti/go_tdd_tutorial.git
+
+go 1.14
+
+require (
+	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/lib/pq v1.7.0 // indirect
+)
